873a4237d8baf7a8cae2fa6ec1098c1eb752f2f6
etaiCWG <142138704+etaiCWG@users.noreply.github.com>
2023-09-19 09:24:11 +0300

    Update 20230316085611.sql

--- migrations/20230316085611.sql
+++ migrations/20230316085611.sql
@@ -4,5 +4,6 @@ CREATE TABLE `users` (
   `user_name` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `etai` varchar(255) NOT NULL,
+  `J` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) CHARSET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
